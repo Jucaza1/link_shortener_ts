ea45290f1de6f5d50093436671b0f265c0eba347
jucaza1 <juan01cz@gmail.com>
2024-10-14 02:11:08 +0200

    added link db service tests

--- tests/db.test.ts
+++ tests/db.test.ts
@@ -214,3 +214,282 @@ describe('Userdb', () => {
         db.teardown()
     })
 })
+describe('Linkdb', () => {
+    const db = createLinkDB()
+    db.teardown()
+    test('createLink', () => {
+        const db = createLinkDB()
+        const link: types.Link = {
+            ID: 0,
+            deleted: false,
+            url: "www.test.com",
+            short: "1234abcd",
+            status: true,
+            userID: "00000000-0000-0000-0000-c677574dfd31",
+            expiresAt: "",
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }
+        const udb = createUserDB()
+        const user1: types.User = {
+            username: "testname1",
+            email: "test1@mail.com",
+            encriptedPassword: "testpassword",
+            ID: "00000000-0000-0000-0000-c677574dfd31",
+            guest: false,
+            deleted: false,
+            isAdmin: false,
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }
+        const resUser1 = udb.createUser(user1)
+        expect(resUser1).toBeDefined()
+        expect(resUser1).toEqual(user1)
+        const resLink = db.createLink(link)
+        expect(resLink).toBeDefined()
+        expect(resLink).toEqual(link)
+        console.log = () => { }
+        db.teardown()
+        udb.teardown()
+    })
+    test('getLinksByUser', () => {
+        const db = createLinkDB()
+        const links: types.Link[] = [{
+            ID: 1,
+            deleted: false,
+            url: "www.test.com",
+            short: "1234abcd",
+            status: true,
+            userID: "00000000-0000-0000-0000-c677574dfd31",
+            expiresAt: "",
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }, {
+            ID: 2,
+            deleted: false,
+            url: "www.test2.com",
+            short: "1234abcd",
+            status: true,
+            userID: "00000000-0000-0000-0000-c677574dfd31",
+            expiresAt: "",
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        },]
+        const udb = createUserDB()
+        const user: types.User = {
+            username: "testname1",
+            email: "test1@mail.com",
+            encriptedPassword: "testpassword",
+            ID: "00000000-0000-0000-0000-c677574dfd31",
+            guest: false,
+            deleted: false,
+            isAdmin: false,
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }
+        const resUser1 = udb.createUser(user)
+        expect(resUser1).toBeDefined()
+        expect(resUser1).toEqual(user)
+        let resLinks: Array<types.Link | undefined> = []
+        resLinks[0] = db.createLink(links[0])
+        resLinks[1] = db.createLink(links[1])
+        expect(resLinks[0]).toBeDefined()
+        expect(resLinks[1]).toBeDefined();
+        links[0].ID = ((resLinks[0] as types.Link).ID) as number
+        links[1].ID = ((resLinks[1] as types.Link).ID) as number
+        expect(resLinks).toEqual(links)
+        const AllLinksGet = db.getLinksByUser(user.ID)
+        expect(AllLinksGet[0]).toBeDefined()
+        expect(AllLinksGet[1]).toBeDefined()
+        expect(AllLinksGet).toEqual(links)
+        console.log = () => { }
+        db.teardown()
+        udb.teardown()
+    })
+    test('getLinkByShort', () => {
+        const db = createLinkDB()
+        const links: types.Link[] = [{
+            ID: 1,
+            deleted: false,
+            url: "www.test.com",
+            short: "1234abcd2",
+            status: true,
+            userID: "00000000-0000-0000-0000-c677574dfd31",
+            expiresAt: "",
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }, {
+            ID: 2,
+            deleted: false,
+            url: "www.test2.com",
+            short: "1234abcd",
+            status: true,
+            userID: "00000000-0000-0000-0000-c677574dfd31",
+            expiresAt: "",
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        },]
+        const udb = createUserDB()
+        const user: types.User = {
+            username: "testname1",
+            email: "test1@mail.com",
+            encriptedPassword: "testpassword",
+            ID: "00000000-0000-0000-0000-c677574dfd31",
+            guest: false,
+            deleted: false,
+            isAdmin: false,
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }
+        const resUser1 = udb.createUser(user)
+        expect(resUser1).toBeDefined()
+        expect(resUser1).toEqual(user)
+        let resLinks: Array<types.Link | undefined> = []
+        resLinks[0] = db.createLink(links[0])
+        resLinks[1] = db.createLink(links[1])
+        expect(resLinks[0]).toBeDefined()
+        expect(resLinks[1]).toBeDefined();
+        links[0].ID = ((resLinks[0] as types.Link).ID) as number
+        links[1].ID = ((resLinks[1] as types.Link).ID) as number
+        expect(resLinks).toEqual(links)
+        const resLink = db.getLinkByShort(links[0].short)
+        expect(resLink).toBeDefined()
+        console.log(new Object(resLink).toString)
+        expect(resLink).toEqual(links[0])
+        console.log = () => { }
+        db.teardown()
+        udb.teardown()
+    })
+    test('getlinkByID', () => {
+        const db = createLinkDB()
+        const links: types.Link[] = [{
+            ID: 0,
+            deleted: false,
+            url: "www.test.com",
+            short: "1234abcd2",
+            status: true,
+            userID: "00000000-0000-0000-0000-c677574dfd31",
+            expiresAt: "",
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }, {
+            ID: 0,
+            deleted: false,
+            url: "www.test2.com",
+            short: "1234abcd",
+            status: true,
+            userID: "00000000-0000-0000-0000-c677574dfd31",
+            expiresAt: "",
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        },]
+        const udb = createUserDB()
+        const user: types.User = {
+            username: "testname1",
+            email: "test1@mail.com",
+            encriptedPassword: "testpassword",
+            ID: "00000000-0000-0000-0000-c677574dfd31",
+            guest: false,
+            deleted: false,
+            isAdmin: false,
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }
+        const resUser1 = udb.createUser(user)
+        expect(resUser1).toBeDefined()
+        expect(resUser1).toEqual(user)
+        let resLinks: Array<types.Link | undefined> = []
+        resLinks[0] = db.createLink(links[0])
+        resLinks[1] = db.createLink(links[1])
+        expect(resLinks[0]).toBeDefined()
+        expect(resLinks[1]).toBeDefined();
+        links[0].ID = ((resLinks[0] as types.Link).ID) as number
+        links[1].ID = ((resLinks[1] as types.Link).ID) as number
+        expect(resLinks).toEqual(links)
+        const resLink = db.getLinkByID(links[0].ID)
+        expect(resLink).toBeDefined()
+        console.log(new Object(resLink).toString)
+        expect(resLink).toEqual(links[0])
+        console.log = () => { }
+        db.teardown()
+        udb.teardown()
+    })
+    test('serveLink', () => {
+        const db = createLinkDB()
+        const link: types.Link = {
+            ID: 0,
+            deleted: false,
+            url: "www.test.com",
+            short: "1234abcd",
+            status: true,
+            userID: "00000000-0000-0000-0000-c677574dfd31",
+            expiresAt: "",
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }
+        const udb = createUserDB()
+        const user1: types.User = {
+            username: "testname1",
+            email: "test1@mail.com",
+            encriptedPassword: "testpassword",
+            ID: "00000000-0000-0000-0000-c677574dfd31",
+            guest: false,
+            deleted: false,
+            isAdmin: false,
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }
+        const resUser1 = udb.createUser(user1)
+        expect(resUser1).toBeDefined()
+        expect(resUser1).toEqual(user1)
+        const resLink = db.createLink(link)
+        expect(resLink).toBeDefined()
+        expect(resLink).toEqual(link)
+        const url = db.serveLink(link.short)
+        expect(url).toBeDefined()
+        expect(url).toEqual(link.url)
+        console.log = () => { }
+        db.teardown()
+        udb.teardown()
+    })
+    test('getLastLinkID', () => {
+        const db = createLinkDB()
+        const link: types.Link = {
+            ID: 0,
+            deleted: false,
+            url: "www.test.com",
+            short: "1234abcd",
+            status: true,
+            userID: "00000000-0000-0000-0000-c677574dfd31",
+            expiresAt: "",
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }
+        const udb = createUserDB()
+        const user1: types.User = {
+            username: "testname1",
+            email: "test1@mail.com",
+            encriptedPassword: "testpassword",
+            ID: "00000000-0000-0000-0000-c677574dfd31",
+            guest: false,
+            deleted: false,
+            isAdmin: false,
+            createdAt: (new Date(Date.now())).toISOString().split("T")[0],
+            deletedAt: ""
+        }
+        const resUser1 = udb.createUser(user1)
+        expect(resUser1).toBeDefined()
+        expect(resUser1).toEqual(user1)
+        const resLink = db.createLink(link)
+        expect(resLink).toBeDefined()
+        link.ID = (resLink as types.Link).ID
+        expect(resLink).toEqual(link)
+        const id = db.getLastLinkID()
+        expect(id).toBeDefined()
+        expect(id).toEqual(link.ID)
+        console.log = () => { }
+        db.teardown()
+        udb.teardown()
+    })
+    //TODO test fail cases -> same: id, username, email
+})
